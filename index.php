769a6f9115b12995c00d691ff6902170349d6fc9
Phellipe Sander dos Santos Guimaraes <pguimaraes@iorj.gov>
2018-07-25 14:09:50 -0300

    erro no cacheusers

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $app->get('/', function() {
 });
 
 //Rota admin
-$app->get('/admin', function() {
+$app->get("/admin", function() {
 
 	User::verifyLogin();
 
@@ -31,8 +31,7 @@ $app->get('/admin', function() {
 });
 
 //rota loginAdmin
-$app->get('/admin/login', function(){
-
+$app->get("/admin/login", function(){
 	$page = new PageAdmin([
 		"header"=>false,
 		"footer"=>false
@@ -42,7 +41,7 @@ $app->get('/admin/login', function(){
 });
 
 //Rota de login admin POST
-$app->post('/admin/login', function(){
+$app->post("/admin/login", function(){
 
 	User::login($_POST["login"], $_POST["password"]);
 
@@ -50,11 +49,56 @@ $app->post('/admin/login', function(){
 	exit;
 });
 
-$app->get('/admin/logout', function()){
+//rota de logout admin
+$app->get("/admin/logout", function(){
 	User::logout();
 	header("Location: /admin/login");
 	exit;
-}
+});
+
+//
+$app->get("/admin/users", function(){
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+});
+//tela de listar usuários
+$app->get("/admin/users/create", function(){
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+});
+
+//deletar usuarios
+$app->get("/admin/users/:iduser/delete", function($iduser){
+	User::verifyLogin();
+});
+//alterar usuarios
+$app->get("/admin/users/:iduser", function($iduser){
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update");
+});
+
+//salvar usuarios no banco
+$app->post("/admin/users/create", function(){
+	User::verifyLogin();
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+	User::verifyLogin();
+});
+
 
 
 $app->run();
